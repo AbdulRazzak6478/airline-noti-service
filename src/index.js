edca3c3543853f7f5ad215b78222d848d0ec949c
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-10-10 20:36:26 +0530

    Created nodemailer tranport object

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const {ServerConfig:{PORT},Logger} = require('./config');
+const {Logger, ServerConfig} = require('./config');
 const apiRoutes =require('./routes');
-
+const mailSender = require('./config/email-config')
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
 
 
-app.listen(PORT,()=>{
-    console.log(`Successfully started the server on PORT ${PORT} `); 
-    Logger.info("Successfully started server"," root ", {});
+app.listen(ServerConfig.PORT,async ()=>{
+    console.log(`Successfully started the server on PORT ${ServerConfig.PORT} `); 
+    Logger.info("Successfully started server", {});
+    try {
+        const response = await mailSender.sendMail({
+            from : ServerConfig.GMAIL_EMAIL,
+            to : 'abc@gmail.com',
+            subject:'Is the service working ? is this working now as well',
+            text : 'Yes it is working'
+        });
+        console.log('mail response : ',response);
+    } catch (error) {
+        console.log('mail error ',error);
+    }
 });
